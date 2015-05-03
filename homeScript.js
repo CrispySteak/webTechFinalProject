dedb31eae3a12d49940a9d281d6082a67dde60c6
ajjoyal01 <ajjoyal01@gmail.com>
2015-05-03 17:23:56 -0400

    Changes

--- homeScript.js
+++ homeScript.js
@@ -6,11 +6,14 @@ var gameCanvas;			//canvas element
 var ctx;				//canvas context
 
 $(document).ready(function(){
-	gameCanvas=document.getElementById("gameCanvas");				//assign canvas on load
-	ctx=gameCanvas.getContext("2d");								//assign canvas
-	gameCanvas.addEventListener('click', gameCanvasClick, false);	//add click listener to canvas
-	ctx.font='20px Arial';											//sent the font of the context for text output (otherwise it is illegible)
-	ctx.fillText("Click to start the game!",50,50);					//the dimensions do not seem to line up with pixels too well
+	
+//	gameCanvas=document.getElementById("gameCanvas");				//assign canvas on load
+//	ctx=gameCanvas.getContext("2d");								//assign canvas
+//	gameCanvas.addEventListener('click', gameCanvasClick, false);	//add click listener to canvas
+//	ctx.font='20px Arial';											//sent the font of the context for text output (otherwise it is illegible)
+//	ctx.fillText("Click to start the game!",50,50);					//the dimensions do not seem to line up with pixels too well
+	
+	
 });
 
 function gameCanvasClick(e)
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<head>
 		<?php 
 			include_once("includes.php");
+			echo "Hello";
 		?>
 		<script src="homeScript.js"></script>	
 	</head>
@@ -20,6 +21,37 @@
 		
 		<!-- The image selection slider at the top of the page -->
 		<div id="image_selection">
+		</div>
+		
+		<div id="game_selection">
+			
+			<div id="image_selection">
+			</div>
+			
+			<div id="game_selection_form">
+				<div id="difficulty_selection">
+					<input type="radio" name="radio" id="radio1" class="radio" checked/>
+					<label for="radio1">Easy</label>
+					</div>
+
+					<div>
+					<input type="radio" name="radio" id="radio2" class="radio"/>
+					<label for="radio2">Medium</label>
+					</div>
+
+					<div>	
+					<input type="radio" name="radio" id="radio3" class="radio"/>
+					<label for="radio3">Hard</label>
+					</div>
+
+					<div>	
+					<input type="radio" name="radio" id="radio4" class="radio"/>
+					<label for="radio4">Expert</label>
+					</div>
+				</div>
+				<button type="button">Timed</button>
+			</div>
+			
 		</div>
 		
 		<!-- The game board itself... think divs within divs, the inner divs are all displaying part of the image and are moveable and will snap -->
